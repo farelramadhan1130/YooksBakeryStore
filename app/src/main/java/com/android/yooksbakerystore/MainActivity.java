be3e2279b0ff00876e47748baeffb7bac5261e82
Akmalusia <ridhoakmal2003@gmail.com>
2023-05-22 20:09:23 +0700

    update tipis

--- app/src/main/java/com/android/yooksbakerystore/MainActivity.java
+++ app/src/main/java/com/android/yooksbakerystore/MainActivity.java
@@ -7,17 +7,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
