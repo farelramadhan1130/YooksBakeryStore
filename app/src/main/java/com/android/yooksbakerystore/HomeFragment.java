0a3140a093b409b03720aa8b714242f54c4d3a86
farelramadhan1130 <113337708+farelramadhan1130@users.noreply.github.com>
2023-06-05 10:34:58 +0700

    ApiTampil Menu

--- app/src/main/java/com/android/yooksbakerystore/HomeFragment.java
+++ app/src/main/java/com/android/yooksbakerystore/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.android.yooksbakerystore;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.yooksbakerystore.Product;
 import com.android.yooksbakerystore.ProductAdapter;
 import com.android.yooksbakerystore.R;
@@ -22,8 +25,8 @@ import com.android.yooksbakerystore.VolleySingleton;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,14 +35,14 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private ProductAdapter productAdapter;
     private List<Product> productList;
-    private static final String API_URL = "http://192.168.1.3:8000/api/tampilmenu"; // Ubah URL sesuai dengan endpoint tampilmenu di server Anda
+    private static final String API_URL = "http://10.125.173.39:8000/api/tampilmenu"; // Ubah URL sesuai dengan endpoint tampilmenu di server Anda
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         recyclerView = view.findViewById(R.id.menu_roti);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
 
         productList = new ArrayList<>();
         productAdapter = new ProductAdapter(getActivity(), productList);
@@ -51,25 +54,34 @@ public class HomeFragment extends Fragment {
     }
 
     private void fetchProducts() {
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, API_URL, null,
-                new Response.Listener<JSONArray>() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, API_URL, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
+                        JSONArray data = null;
                         try {
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject productObject = response.getJSONObject(i);
-                                int idProduk = productObject.getInt("id_produk");
-                                int hargaJual = productObject.getInt("harga_jual");
-                                int hargaCoret = productObject.getInt("harga_coret");
-                                int stok = productObject.getInt("stok");
-                                String nama = productObject.getString("nama");
-                                String deskripsiProduk = productObject.getString("deskripsi_produk");
-                                String fotoProduk = productObject.getString("foto_produk");
+                            data = response.getJSONArray("data");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        try {
+//                            ArrayList<Product> productList = new ArrayList<>();
+
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject dataObject = data.getJSONObject(i);
+
+                                int idProduk = dataObject.getInt("id_produk");
+                                int hargaJual = dataObject.getInt("jual_produk");
+                                int hargaCoret = dataObject.getInt("harga_coret");
+                                int stok = dataObject.getInt("stock_produk");
+                                String nama = dataObject.getString("nama_produk");
+                                String deskripsiProduk = dataObject.getString("keterangan_produk");
+                                String fotoProduk = dataObject.getString("foto_produk");
 
                                 Product product = new Product(idProduk, hargaJual, hargaCoret, stok, nama, deskripsiProduk, fotoProduk);
                                 productList.add(product);
                             }
-
                             productAdapter.notifyDataSetChanged();
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -84,6 +96,7 @@ public class HomeFragment extends Fragment {
                             errorMessage += ": " + error.getMessage();
                         }
                         if (getActivity() != null) {
+                            Log.e(TAG, "onErrorResponse: "+errorMessage);
                             Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
                         }
                     }
---
+++ app/src/main/java/com/android/yooksbakerystore/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.android.yooksbakerystore;
+
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.annotation.GlideModule;
+
+public class MyAppGlideModule extends AppGlideModule {
+    // Kosongkan saja atau tambahkan konfigurasi tambahan jika diperlukan
+}
